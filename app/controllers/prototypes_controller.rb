3f3dfddcb8c4a29af575ef92edff832f8399e2ab
small-bear-a <77313049+small-bear-a@users.noreply.github.com>
2021-02-08 21:43:36 +0900

    protospace作成

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,54 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :edit, :destroy]
+
+  def index
+    @prototypes =Prototype.all .order(id: "desc")
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(message)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(message)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def message
+    params.require(:prototype). permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+end
---
+++ db/migrate/20210206050034_create_prototypes.rb
@@ -0,0 +1,11 @@
+class CreatePrototypes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prototypes do |t|
+      t.string :title, null: false
+      t.text :catch_copy, null: false
+      t.text :concept, null: false
+      t.references :user, forign_key: true, null:false
+      t.timestamps
+    end
+  end
+end
